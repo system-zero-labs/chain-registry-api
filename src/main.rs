30c96a979b3c1f7048b3f61088b2738400f18d1c
David Nix <hello@davidnix.io>
2023-03-19 09:57:17 -0600

    Initial clap

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
+use clap::{Parser, Subcommand};
+
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+struct Cli {
+}
+
 fn main() {
-    println!("Hello, world!");
+    let cli = Cli::parse();
+    println!("{:?}", cli)
 }
