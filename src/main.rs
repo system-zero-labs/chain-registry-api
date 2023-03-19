a5978f5ee01ba07a3dd9a012eb3078ee93c00bfb
David Nix <hello@davidnix.io>
2023-03-19 10:18:50 -0600

    Server command

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,14 @@
-use clap::{Parser, Subcommand};
+use clap::{Parser};
 
 #[derive(Parser, Debug)]
 #[command(author, version, about, long_about = None)]
 struct Cli {
+    #[arg(short, long, default_value = "8765", help = "Port to bind", env = "PORT")]
+    port: u16,
 }
+
 
 fn main() {
     let cli = Cli::parse();
-    println!("{:?}", cli)
+    println!("{:?}", cli);
 }
