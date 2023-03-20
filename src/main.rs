8ae87228fb1288dac135e3634066dd1410f20221
David Nix <hello@davidnix.io>
2023-03-19 20:51:38 -0600

    Hydrate scaffold

--- src/main.rs
+++ src/main.rs
@@ -2,28 +2,32 @@ use clap::{Parser, Subcommand};
 
 #[derive(Parser, Debug)]
 #[command(author, version, about, long_about = None)]
-struct Cli {
+struct Args {
     #[command(subcommand)]
     sub: Sub,
 }
 
 #[derive(Subcommand, Debug)]
 enum Sub {
+
     #[command(about = "Run the API server")]
     Serve {
         #[arg(short, long, default_value = "8675", help = "Port to bind", env = "PORT")]
         port: u16,
     },
+
     #[command(about = "Download data from Chain Registry and store in database")]
-    Hydrate {},
+    Hydrate {
+        #[arg(long, default_value = "https://github.com/cosmos/chain-registry", help = "Chain Registry git URL")]
+        git_remote: String,
+    },
 }
 
 fn main() {
-    let cli = Cli::parse();
-    println!("{:?}", cli);
+    let cli = Args::parse();
 
     match cli.sub {
         Sub::Serve { port } => println!("Serving on port {}", port),
-        Sub::Hydrate {} => println!("Hydrating: TODO"),
+        Sub::Hydrate { git_remote} => println!("Hydrating using {}: TODO", git_remote),
     }
 }
