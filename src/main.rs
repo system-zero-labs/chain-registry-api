94836a7d1c6b20cb380f79213d7b988f4c27f977
David Nix <hello@davidnix.io>
2023-03-19 20:22:59 -0600

    Stub commands

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,29 @@
-use clap::{Parser};
+use clap::{Parser, Subcommand};
 
 #[derive(Parser, Debug)]
 #[command(author, version, about, long_about = None)]
 struct Cli {
-    #[arg(short, long, default_value = "8765", help = "Port to bind", env = "PORT")]
-    port: u16,
+    #[command(subcommand)]
+    sub: Sub,
 }
 
+#[derive(Subcommand, Debug)]
+enum Sub {
+    #[command(about = "Run the API server")]
+    Serve {
+        #[arg(short, long, default_value = "8675", help = "Port to bind", env = "PORT")]
+        port: u16,
+    },
+    #[command(about = "Download data from Chain Registry and store in database")]
+    Hydrate {},
+}
 
 fn main() {
     let cli = Cli::parse();
     println!("{:?}", cli);
+
+    match cli.sub {
+        Sub::Serve { port } => println!("Serving on port {}", port),
+        Sub::Hydrate {} => println!("Hydrating: TODO"),
+    }
 }
