b050713e1b0698a2abd4eee6d0aa8df78933081c
David Nix <hello@davidnix.io>
2023-03-31 15:49:07 -0600

    Add sqlx build script workaround

--- migrations/20230331193302_truncate_chain_trigger.sql
+++ migrations/20230331193302_truncate_chain_trigger.sql
@@ -1,13 +1,18 @@
 CREATE OR REPLACE FUNCTION truncate_chains()
-    RETURNS TRIGGER AS $$
+    RETURNS TRIGGER AS
+$$
 BEGIN
-    DELETE FROM chain
-    WHERE id NOT IN (
-        SELECT id FROM chain ORDER BY created_at DESC LIMIT 10000
-    );
+    DELETE
+    FROM chain
+    WHERE id NOT IN (SELECT id
+                     FROM chain
+                     ORDER BY created_at DESC
+                     LIMIT 10000);
 END;
 $$ LANGUAGE plpgsql;
 
 CREATE TRIGGER limit_chain_rows
-    AFTER INSERT ON chain
-    FOR EACH ROW EXECUTE FUNCTION truncate_chains();
+    AFTER INSERT
+    ON chain
+    FOR EACH ROW
+EXECUTE FUNCTION truncate_chains();
