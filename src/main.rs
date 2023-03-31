6a7308ba1a2ee1bba2502c29ded8412223bcfed4
David Nix <hello@davidnix.io>
2023-03-31 08:29:35 -0600

    Remove subcommands

--- src/main.rs
+++ src/main.rs
@@ -5,52 +5,40 @@ use tempfile::TempDir;
 #[derive(Parser, Debug)]
 #[command(author, version, about, long_about = None)]
 struct Args {
-    #[command(subcommand)]
-    sub: Sub,
-}
+    #[arg(
+        long,
+        default_value = "https://github.com/cosmos/chain-registry",
+        help = "Chain Registry git URL"
+    )]
+    git_remote: String,
 
-#[derive(Subcommand, Debug)]
-enum Sub {
-    #[command(about = "Run the API server")]
-    Serve {
-        #[arg(
-            short,
-            long,
-            default_value = "8675",
-            help = "Port to bind",
-            env = "PORT"
-        )]
-        port: u16,
-    },
+    #[arg(long, default_value = "master", help = "Git branch or tag")]
+    git_ref: String,
 
-    #[command(about = "Download data from Chain Registry and store in database")]
-    Hydrate {
-        #[arg(
-            long,
-            default_value = "https://github.com/cosmos/chain-registry",
-            help = "Chain Registry git URL"
-        )]
-        git_remote: String,
+    #[arg(long, help = "Path to dir for git clone", required = false)]
+    clone_path: Option<String>,
 
-        #[arg(long, default_value = "master", help = "Git branch or tag")]
-        git_ref: String,
-
-        #[arg(long, help = "Path to dir for git clone", required = false)]
-        path: Option<String>,
-    },
+    #[arg(
+        short,
+        long,
+        default_value = "8675",
+        help = "Port to bind",
+        env = "PORT"
+    )]
+    port: u16,
 }
 
 fn main() {
     let cli = Args::parse();
 
-    match cli.sub {
-        Sub::Serve { port } => println!("Serving on port {}", port),
-        Sub::Hydrate {
-            git_remote,
-            git_ref,
-            path,
-        } => hydrate_chain_registry(git_remote, git_ref, path),
-    }
+    // match cli.sub {
+    //     Sub::Serve { port } => println!("Serving on port {}", port),
+    //     Sub::Hydrate {
+    //         git_remote,
+    //         git_ref,
+    //         path,
+    //     } => hydrate_chain_registry(git_remote, git_ref, path),
+    // }
 }
 
 fn hydrate_chain_registry(remote: String, git_ref: String, path: Option<String>) {
