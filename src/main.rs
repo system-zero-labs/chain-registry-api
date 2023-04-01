856d529d677ef66fa0fa159c73a3cc758c37e443
David Nix <hello@davidnix.io>
2023-04-01 08:09:00 -0600

    Implement hydrate subcommand

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use clap::{Parser, Subcommand};
 mod hydrate;
+use sqlx::postgres::{PgPool, PgPoolOptions};
 use tempfile::TempDir;
 
 #[derive(Parser, Debug)]
@@ -44,26 +45,51 @@ enum Sub {
 async fn main() {
     let cli = Args::parse();
 
+    let pool = PgPoolOptions::new()
+        .max_connections(2)
+        .connect(std::env::var("DATABASE_URL").unwrap().as_str())
+        .await
+        .expect("Failed to connect to database");
+
+    sqlx::migrate!()
+        .run(&pool)
+        .await
+        .expect("Failed to run migrations");
+
     match cli.sub {
         Sub::Serve { port } => println!("Serving on port {}", port),
         Sub::Hydrate {
             git_remote,
             git_ref,
             path,
-        } => hydrate_chain_registry(git_remote, git_ref, path).await,
+        } => hydrate_chain_registry(pool, git_remote, git_ref, path).await,
     }
 }
 
-async fn hydrate_chain_registry(remote: String, git_ref: String, path: Option<String>) {
+async fn hydrate_chain_registry(
+    pool: PgPool,
+    remote: String,
+    git_ref: String,
+    path: Option<String>,
+) {
     let clone_dir =
         path.unwrap_or_else(|| TempDir::new().unwrap().path().to_str().unwrap().to_string());
     println!("Cloning {} {} into {}...", remote, git_ref, clone_dir);
     let chains =
         hydrate::shallow_clone(remote, git_ref, clone_dir.into()).expect("shallow clone failed");
+
+    let mut conn = pool.acquire().await.unwrap();
+
     for chain in chains.mainnets {
-        match hydrate::save_chain(todo, chain.to_path_buf(), "mainnet".to_string()).await {
+        match hydrate::save_chain(&mut conn, chain.to_path_buf(), "mainnet".to_string()).await {
             Ok(_) => {}
             Err(err) => println!("Failed to save mainnet chain {:?}: {:?}", chain, err),
+        }
+    }
+    for chain in chains.testnets {
+        match hydrate::save_chain(&mut conn, chain.to_path_buf(), "testnet".to_string()).await {
+            Ok(_) => {}
+            Err(err) => println!("Failed to save testnet chain {:?}: {:?}", chain, err),
         }
     }
 }
