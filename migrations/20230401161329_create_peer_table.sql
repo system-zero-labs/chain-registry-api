55925e85253a0a65411d2f5475656e338da71be9
David Nix <hello@davidnix.io>
2023-04-03 17:54:00 -0600

    Remove unused column

--- migrations/20230401161329_create_peer_table.sql
+++ migrations/20230401161329_create_peer_table.sql
@@ -3,7 +3,6 @@ CREATE TABLE peer
     id          BIGSERIAL PRIMARY KEY,
     type        TEXT        NOT NULL, -- 'seed' or 'persistent'
     address     TEXT        NOT NULL,
-    provider    TEXT        NOT NULL DEFAULT 'unknown',
     is_alive    BOOLEAN     NOT NULL,
     chain_id_fk BIGINT      NOT NULL REFERENCES chain (id) ON DELETE CASCADE,
     created_at  TIMESTAMPTZ NOT NULL DEFAULT NOW()
