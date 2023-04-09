23152d93f0ec53d5161db4048d5c10e4f403f66d
David Nix <hello@davidnix.io>
2023-04-09 10:37:16 -0600

    Implement API error

--- src/api/chain.rs
+++ src/api/chain.rs
@@ -1,16 +1,16 @@
+use crate::api::{from_db_error, internal_error, APIError};
 use crate::db::chain;
-use axum::{extract::Path, extract::State, http::StatusCode, Json};
+use axum::{extract::Path, extract::State, Json};
 use sqlx::postgres::PgPool;
 
 pub async fn get_chain_data(
     State(pool): State<PgPool>,
     Path((chain_name, network)): Path<(String, String)>,
-) -> Json<serde_json::Value> {
-    // TODO: error handling
-    let mut conn = pool.acquire().await.unwrap();
+) -> Result<Json<serde_json::Value>, APIError> {
+    let mut conn = pool.acquire().await.map_err(internal_error)?;
     let chain_data = chain::find_chain(&mut conn, chain_name.as_str(), network.as_str())
         .await
-        .unwrap();
+        .map_err(from_db_error)?;
 
-    Json(chain_data.chain_data)
+    Ok(Json(chain_data.chain_data))
 }
--- src/api/mod.rs
+++ src/api/mod.rs
@@ -1,1 +1,36 @@
+use axum::{
+    http::StatusCode,
+    response::{IntoResponse, Response},
+    Json,
+};
+use serde_json::json;
+
 pub(crate) mod chain;
+
+#[derive(Debug)]
+pub enum APIError {
+    NotFound,
+    InternalServerError(String),
+}
+
+fn internal_error(err: impl std::fmt::Display) -> APIError {
+    APIError::InternalServerError(err.to_string())
+}
+
+fn from_db_error(err: sqlx::Error) -> APIError {
+    match err {
+        sqlx::Error::RowNotFound => APIError::NotFound,
+        _ => APIError::InternalServerError(err.to_string()),
+    }
+}
+
+impl IntoResponse for APIError {
+    fn into_response(self) -> Response {
+        let (status, message) = match self {
+            APIError::NotFound => (StatusCode::NOT_FOUND, "Resource not found".to_string()),
+            APIError::InternalServerError(err) => (StatusCode::INTERNAL_SERVER_ERROR, err),
+        };
+        let body = json!({ "error": message });
+        (status, Json(body)).into_response()
+    }
+}
